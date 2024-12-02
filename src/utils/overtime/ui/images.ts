1317514721a03501469b8db93a6877e10b6a211b
jbreite <joshbreite@gmail.com>
2024-12-02 11:20:24 -0500

    bug: fix images with lowercase leagle labe

--- src/utils/overtime/ui/images.ts
+++ src/utils/overtime/ui/images.ts
@@ -3,20 +3,17 @@ import { getLeagueLabel } from "./sportsHelpers";
 import { SUPPORTED_LEAGUES } from "@/app/constants/Constants";
 
 export const getTeamImageSource = (team: string, league: LeagueEnum) => {
-  let leagueLabel = getLeagueLabel(league);
-  const baseUrl = process.env.NEXT_PUBLIC_URL || '';
+  let leagueLabel = getLeagueLabel(league).toLowerCase();
 
-  if (leagueLabel === "NCAA Football") {
+  if (leagueLabel === "ncaa football") {
     leagueLabel = "ncaaf";
   }
 
-  // If league is not supported, return default logo
   if (!SUPPORTED_LEAGUES.includes(league)) {
-    return `${baseUrl}/logos/default.png`;
+    return `/logos/default.png`;
   }
 
-  // For supported leagues, return the standardized path
-  return `${baseUrl}/logos/${leagueLabel}/${team
+  return `/logos/${leagueLabel}/${team
     .trim()
     .replaceAll(" ", "-")
     .replaceAll("/", "-")
