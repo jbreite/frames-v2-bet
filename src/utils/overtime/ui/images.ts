0ef325f09d9adf2b598ca0105f9f804ac0a63fcb
jbreite <joshbreite@gmail.com>
2024-12-02 11:14:33 -0500

    bug: fix images

--- src/utils/overtime/ui/images.ts
+++ src/utils/overtime/ui/images.ts
@@ -4,6 +4,7 @@ import { SUPPORTED_LEAGUES } from "@/app/constants/Constants";
 
 export const getTeamImageSource = (team: string, league: LeagueEnum) => {
   let leagueLabel = getLeagueLabel(league);
+  const baseUrl = process.env.NEXT_PUBLIC_URL || '';
 
   if (leagueLabel === "NCAA Football") {
     leagueLabel = "ncaaf";
@@ -11,11 +12,11 @@ export const getTeamImageSource = (team: string, league: LeagueEnum) => {
 
   // If league is not supported, return default logo
   if (!SUPPORTED_LEAGUES.includes(league)) {
-    return "/logos/default.png";
+    return `${baseUrl}/logos/default.png`;
   }
 
   // For supported leagues, return the standardized path
-  return `/logos/${leagueLabel}/${team
+  return `${baseUrl}/logos/${leagueLabel}/${team
     .trim()
     .replaceAll(" ", "-")
     .replaceAll("/", "-")
